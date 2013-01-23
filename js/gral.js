8a9832f9a2268ff91d514ee7516cd2efb33e8aeb
Leandro Salar <info@reiatsu.com.ar>
2013-01-23 00:06:52 -0300

    Agregue la instancia de ver bal bla
    blsd

--- js/gral.js
+++ js/gral.js
@@ -96,6 +96,122 @@ function funcionesDeCarga()
             alert("el cobro no fue cargado");
     });
 
+    // Comienza el formulario en vio de cargar Cobro
+    $("#resumen").click(function(){
+        var celular =  $("#guardaCelular").html();
+            habilit = $(this).attr("id");
+
+        desHabilitaId(habilit);
+        $.ajax({
+                type:'POST',
+                url:$url,
+                data: 'h=resumen&celular='+celular,
+                dataType:'json',
+                success: function(v)
+                {
+                    $.mobile.changePage( "#etapa2" );
+                    
+                    $i=0;
+                    $ht = '';
+                    bandera1 = 0;
+                    bandera2 = 0;
+                    bandera3 = 0;
+                    bandera4 = 0;
+                    bandera5 = 0;
+                    bandera6 = 0;
+                    bandera7 = 0;
+                    bandera8 = 0;
+                    bandera9 = 0;
+                    bandera10 = 0;
+                    bandera11 = 0;
+                    bandera12 = 0;
+
+                    for($i=0; $i < v['vueltas']; $i++)
+                    {
+                        if(v[$i]['que'] == 'agregados')
+                        {
+                            style = 'colorAgregado';
+                        }
+                        else
+                        {
+                            style = 'colorGasto';
+                        }
+
+                            if(v[$i]['mes'] == 1 && bandera1 == 0)
+                            {
+                                        bandera1 =1;
+                                        $ht += '<div class="floatLeft w100 titulito">ENERO</div>';
+                            }
+                            if(v[$i]['mes'] == 2 && bandera2 == 0)
+                            {           
+                                        bandera2 = 1;
+                                        $ht += '<div class="floatLeft w100 titulito">FEBRERO</div>';
+                            }
+                            if(v[$i]['mes'] == 3 && bandera3 == 0)
+                            {
+                                        bandera3 = 1
+                                        $ht += '<div class="floatLeft w100 titulito">MARZO</div>';
+                            }
+                            if(v[$i]['mes'] == 4 && bandera4 == 0)
+                            {
+                                        bandera4 =1
+                                        $ht += '<div class="floatLeft w100 titulito">ABRIL</div>';
+                            }
+                            if(v[$i]['mes'] == 5 && bandera5 == 0)
+                            {
+                                        bandera5 = 1
+                                        $ht += '<div class="floatLeft w100 titulito">MAYO</div>';
+                            }
+                            if(v[$i]['mes'] == 6  && bandera6 == 0)
+                            {
+                                        bandera3 = 6
+                                        $ht += '<div class="floatLeft w100 titulito">JUNIO</div>';
+                            }
+                            if(v[$i]['mes'] == 7 && bandera7 == 0)
+                            {
+                                        bandera7 = 7
+                                        $ht += '<div class="floatLeft w100 titulito">JULIO</div>';
+                            }
+                            if(v[$i]['mes'] == 8  && bandera8 == 0)
+                            {
+                                        bandera8 = 1
+                                        $ht += '<div class="floatLeft w100 titulito">AGOSTO</div>';
+                            }
+                            if(v[$i]['mes'] == 9 && bandera9 == 0)
+                            {
+                                        bandera9 = 1
+                                        $ht += '<div class="floatLeft w100 titulito">SEPTIEMBRE</div>';
+                            }
+                            if(v[$i]['mes'] == 10  && bandera10 == 0)
+                           {
+                                        bandera10 = 1
+                                        $ht += '<div class="floatLeft w100 titulito">OCTUBRE</div>';
+                           }
+                            if(v[$i]['mes'] == 11 && bandera11 == 0)
+                           {
+                                        bandera11 == 1
+                                        $ht += '<div class="floatLeft w100 titulito">NOVIEMBRE</div>';
+                           }
+                            if(v[$i]['mes'] == 12 && bandera12 == 0)
+                           {
+                                        bandera12 = 1
+                                        $ht += '<div class="floatLeft w100 titulito">DICIEMBRE</div>';
+                           }
+
+                            $ht += '<div class="floatLeft w100 cuerpito '+style+'">\
+                                    <div class="floatLeft w70">'+v[$i]['de']+'</div>\
+                                    <div class="floatLeft w30">'+v[$i]['monto']+'</div>\
+                                    </div>';
+                    }
+                    
+                    $("#muestraResumen").html($ht);
+
+                    limpiarInputs();
+                    habilitaId(habilit);
+                }
+            });
+    });
+
     // envia los datos a la base para saber quien hace las transferencias y donde 
     $("#validar").click(function(){
         var celular = parseInt($("input[name=numero]").val());
